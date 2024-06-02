9413eae17995b2ab69969697b4a05c9f8791b2c4
Constantin <beloglazov.constantin@gmail.com>
2024-06-03 01:55:04 +0300

    dev: added tests for recipe

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Collection;
 use \Illuminate\Http\JsonResponse;
 use App\Models\Recipe;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 final class RecipeController extends Controller
 {
