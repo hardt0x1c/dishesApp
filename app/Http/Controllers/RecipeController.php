08c9fd774e1e70dfa9227142eda8621fe5b902bf
Constantin <beloglazov.constantin@gmail.com>
2024-06-03 00:16:16 +0300

    feat: added recipe controlled and routes

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
+use \Illuminate\Http\JsonResponse;
+use App\Models\Recipe;
+
+final class RecipeController extends Controller
+{
+    public function index(): Collection
+    {
+        return Recipe::all();
+    }
+
+    public function show(int $id): Recipe
+    {
+        return Recipe::findOrFail($id);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $recipeData = [
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'instructions' => $request->input('instructions'),
+        ];
+
+        $result = Recipe::create($recipeData);
+
+        if ($result) {
+            return response()->json(['message' => 'Recipe created successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Recipe not created'], 500);
+        }
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $recipeData = [
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'instructions' => $request->input('instructions'),
+        ];
+
+        $result = Recipe::where('id', $id)->update($recipeData);
+
+        if ($result) {
+            return response()->json(['message' => 'Recipe updated successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Recipe not found'], 404);
+        }
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $result = Recipe::destroy($id);
+
+        if ($result) {
+            return response()->json(['message' => 'Recipe deleted successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Recipe not found'], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+Route::get('/recipe', [RecipeController::class, 'index']);
+Route::get('/recipe/{id}', [RecipeController::class, 'show']);
+Route::post('/recipe', [RecipeController::class, 'store']);
+Route::put('/recipe/{id}', [RecipeController::class, 'update']);
+Route::delete('/recipe/{id}', [RecipeController::class, 'destroy']);
